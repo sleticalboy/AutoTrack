d1ddbe9b735eff129600192ad712c84fe8660d41
sleticalboy <sleticalboy@gmail.com>
2022-05-24 18:39:53 +0800

    feat: 通过 apt 工具链尝试生成 java 文件详细信息

--- javadoc-processor/src/main/java/com/binlee/javadoc/extractor/JavadocPrinter.java
+++ javadoc-processor/src/main/java/com/binlee/javadoc/extractor/JavadocPrinter.java
@@ -1,6 +1,9 @@
 package com.binlee.javadoc.extractor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
 import javax.lang.model.element.Element;
@@ -12,6 +15,7 @@ import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.SimpleElementVisitor8;
+import jdk.nashorn.internal.objects.NativeJSON;
 
 /**
  * Created on 2022/5/18
@@ -22,18 +26,61 @@ final class JavadocPrinter extends SimpleElementVisitor8<JavadocPrinter, Object>
 
   private final Elements mUtils;
   private final String mTag;
-
-  private JavadocPrinter(String tag, Elements utils) {
-    mTag = tag;
-    mUtils = utils;
-  }
+  private final Map<String, Object> mJson;
 
   public static void print(Set<? extends Element> elements, Elements utils, String tag) {
     Log.e("JavadocPrinter", "print() elements: " + elements);
     if (elements == null || elements.size() == 0) return;
     for (Element e : elements) {
-      new JavadocPrinter(tag, utils).visit(e);
+      Log.i("JavadocPrinter", "visit " + e + " ---> start --->");
+      Log.i("JavadocPrinter", "visit " + e + " ---> start --->");
+      // class info:
+      //{
+      //  'super_class': 'java.lang.Object',
+      //  'interfaces': [
+      //    'java.util.concurrent.Callable<java.util.List<java.lang.String>>',
+      //    'java.util.concurrent.Future<com.sleticalboy.transform.bean.Image>',
+      //    'java.lang.Runnable',
+      //  ]
+      //  'constructors': [
+      //    '', ...
+      //  ]
+      //  methods: [
+      //    {
+      //      '创建时被调用': '//'
+      //      'name': 'onCreate',
+      //      'params': [
+      //        {
+      //          '状态': '//',
+      //          'savedInstanceState': 'android.os.Bundle',
+      //        },
+      //        {
+      //          '超时时长': '//',
+      //          'timeout': 'L'
+      //        },
+      //        {
+      //          '时间单位': '//',
+      //          'unit': 'java.util.concurrent.TimeUnit'
+      //        }
+      //      ],
+      //      'throws': [
+      //        'java.util.concurrent.ExecutionException',
+      //        'java.lang.InterruptedException'
+      //      ]
+      //    }
+      //  ]
+      //}
+      Map<String, Object> json = new HashMap<>();
+      new JavadocPrinter(tag, utils, json).visit(e);
+      Log.i("JavadocPrinter", "visit " + e + " ---> end --->");
+      Log.i("JavadocPrinter", "visit " + e + " ---> end --->");
     }
+  }
+
+  private JavadocPrinter(String tag, Elements utils, Map<String, Object> json) {
+    mTag = tag;
+    mUtils = utils;
+    mJson = json;
   }
 
   // 打印文档信息
@@ -69,14 +116,17 @@ final class JavadocPrinter extends SimpleElementVisitor8<JavadocPrinter, Object>
     buf.append(e.asType());
     // 父类及接口
     final TypeMirror superclass = e.getSuperclass();
+    mJson.put("super_class", superclass.toString());
     buf.append(" extends ").append(superclass);
     final List<? extends TypeMirror> interfaces = e.getInterfaces();
     if (interfaces.size() != 0) {
       buf.append(" implements ");
+      List<String> interfaceList = new ArrayList<>();
       for (int i = 0, size = interfaces.size(); i < size; i++) {
         buf.append(interfaces.get(i));
         if (i != size - 1) buf.append(", ");
       }
+      mJson.put("interfaces", interfaceList);
     }
     buf.append(" {}");
     Log.w(mTag, buf.toString());
